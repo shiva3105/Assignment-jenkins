728de195694cacbba980171acd7d178059ef4cf2
shiva3105 <shivajinm1010@gmail.com>
2026-04-06 00:50:03 +0530

    Adding big2 - checking assignment work

---
+++ big2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 2 ];then
+        echo "pass 2 args only"
+        exit 1
+fi
+
+if [ $1 -gt $2 ];then
+        echo "$1 is big"  # 1st arguemnt
+else
+        echo "$2 is big"
+fi
